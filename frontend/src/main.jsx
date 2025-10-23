181d5e683887ace4704b36963f2f1a477dc91fd7
Chinmay Sawant <sawantchinmay040@gmail.com>
2025-10-24 01:30:01 +0530

    Splitted style.css into small components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './styles.css'
+import './css/theme.css'
+import './css/nav.css'
+import './css/layout.css'
+import './css/components.css'
+import './css/pages.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
